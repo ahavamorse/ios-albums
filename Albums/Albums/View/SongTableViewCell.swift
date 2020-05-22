5205497bccce52f9d5e52c066e587ab8e6976a7e
ahavamorse <ahavamorse@icloud.com>
2020-05-22 10:30:34 -0700

    AlbumDetailViewController set up

---
+++ Albums/Albums/View/SongTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  SongTableViewCell.swift
+//  Albums
+//
+//  Created by Ahava on 5/22/20.
+//  Copyright © 2020 Ahava. All rights reserved.
+//
+
+import UIKit
+
+class SongTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var songTitleTextField: UITextField!
+    @IBOutlet weak var durationTextField: UITextField!
+    
+    @IBAction func addSong(_ sender: Any) {
+        
+    }
+}
