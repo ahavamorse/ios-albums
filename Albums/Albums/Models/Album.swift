cbe8fb3742a18eadf2c0d54c952c5871a330b8d9
ahavamorse <ahavamorse@icloud.com>
2020-05-22 11:28:30 -0700

    Decoding Albums now works

---
+++ Albums/Albums/Models/Album.swift
@@ -0,0 +1,104 @@
+//
+//  Album.swift
+//  Albums
+//
+//  Created by Ahava on 5/22/20.
+//  Copyright © 2020 Ahava. All rights reserved.
+//
+
+import Foundation
+
+struct Album: Decodable {
+    
+    enum CodingKeys: String, CodingKey {
+        case artist
+        case name
+        case id
+        case genres
+        case songs
+        case coverArt
+        
+        enum SongsKeys: String, CodingKey {
+            case duration
+            case id
+            case name
+            
+            enum DurationKeys: String, CodingKey {
+                case duration
+            }
+            
+            enum NameKeys: String, CodingKey {
+                case title
+            }
+        }
+        
+        enum CoverArtKeys: String, CodingKey {
+            case url
+        }
+    }
+    
+    var name: String
+    var artist: String
+    var id: String
+    var genres: [String]
+    var songs: [Song]
+    var coverArt: [URL]
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        // Decode name, artist, and id
+        self.name = try container.decode(String.self, forKey: .name)
+        self.artist = try container.decode(String.self, forKey: .artist)
+        self.id = try container.decode(String.self, forKey: .id)
+        
+        // Decode Genres
+        var genresContainer = try container.nestedUnkeyedContainer(forKey: .genres)
+        var genres: [String] = []
+        
+        while genresContainer.isAtEnd == false {
+            let genre = try genresContainer.decode(String.self)
+            genres.append(genre)
+        }
+        
+        self.genres = genres
+        
+        // Decode Songs
+        var songsContainer = try container.nestedUnkeyedContainer(forKey: .songs)
+        var songs: [Song] = []
+        
+        while songsContainer.isAtEnd == false {
+            let songContainer = try songsContainer.nestedContainer(keyedBy: CodingKeys.SongsKeys.self)
+            
+            let songId = try songContainer.decode(String.self, forKey: .id)
+            
+            let durationContainer = try songContainer.nestedContainer(keyedBy: CodingKeys.SongsKeys.DurationKeys.self, forKey: .duration)
+            
+            let songDuration = try durationContainer.decode(String.self, forKey: .duration)
+            
+            let nameContainer = try songContainer.nestedContainer(keyedBy: CodingKeys.SongsKeys.NameKeys.self, forKey: .name)
+            
+            let songTitle = try nameContainer.decode(String.self, forKey: .title)
+            
+            let song = Song(title: songTitle, duration: songDuration, id: songId)
+            songs.append(song)
+        }
+        
+        self.songs = songs
+        
+        // Decode Cover Art
+        var coverArtContainer = try container.nestedUnkeyedContainer(forKey: .coverArt)
+        var urls: [URL] = []
+        
+        while coverArtContainer.isAtEnd == false {
+            let urlContainer = try coverArtContainer.nestedContainer(keyedBy: CodingKeys.CoverArtKeys.self)
+            
+            let urlString = try urlContainer.decode(String.self, forKey: .url)
+            if let url = URL(string: urlString) {
+                urls.append(url)
+            }
+        }
+        
+        self.coverArt = urls
+    }
+}
---
+++ Albums/Albums/Models/Song.swift
@@ -0,0 +1,15 @@
+//
+//  Song.swift
+//  Albums
+//
+//  Created by Ahava on 5/22/20.
+//  Copyright © 2020 Ahava. All rights reserved.
+//
+
+import Foundation
+
+struct Song: Decodable {
+    var title: String
+    var duration: String
+    var id: String
+}
