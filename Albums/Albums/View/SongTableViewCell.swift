1715574a3c1617ee82a9d6bd63cd1a66f94ecdfc
ahavamorse <ahavamorse@icloud.com>
2020-06-05 09:20:20 -0700

    Implemented SongTableViewCell

--- Albums/Albums/View/SongTableViewCell.swift
+++ Albums/Albums/View/SongTableViewCell.swift
@@ -12,8 +12,35 @@ class SongTableViewCell: UITableViewCell {
 
     @IBOutlet weak var songTitleTextField: UITextField!
     @IBOutlet weak var durationTextField: UITextField!
+    @IBOutlet weak var addSongButton: UIButton!
+    
+    var song: Song?
+    var delegate: SongTableViewCellDelegate?
+    
+    func update() {
+        if let song = song {
+            songTitleTextField.text = song.title
+            durationTextField.text = song.duration
+            addSongButton.isHidden = true
+        }
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        songTitleTextField.text = ""
+        durationTextField.text = ""
+        addSongButton.isHidden = false
+    }
     
     @IBAction func addSong(_ sender: Any) {
-        
+        if let title = songTitleTextField.text,
+            let duration = durationTextField.text {
+            delegate?.addSong(with: title, duration: duration)
+        }
     }
+}
+
+protocol SongTableViewCellDelegate {
+    func addSong(with title: String, duration: String)
 }
